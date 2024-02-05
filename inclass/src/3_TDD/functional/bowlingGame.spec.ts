4b47b6b56f94aa73bf427d03eb2ccb68b4ad9ad4
Desiree Mac <audny.correac@gmail.com>
2024-02-05 12:46:30 -0600

    feat: functional TDD

--- inclass/src/3_TDD/functional/bowlingGame.spec.ts
+++ inclass/src/3_TDD/functional/bowlingGame.spec.ts
@@ -1,8 +1,52 @@
+import { Game, createBowlingGame } from "./bowlingGame";
 
-describe('Test bowling game', () => {
+describe("Test bowling game functional", () => {
+  let game: Game;
+  beforeEach(() => {
+    game = createBowlingGame();
+  });
 
-  it('dummy test', () => {
-    expect(1).toBe(1)
-  })
+  it("should roll a ball", () => {
+    game = game.roll(0);
+    expect(game).toBeDefined();
+  });
 
+  it("should play a gutter game", () => {
+    game = rollMany(game, 20, 0);
+    expect(game.score()).toBe(0);
+  });
+
+  it("should roll all once", () => {
+    game = rollMany(game, 20, 1);
+    expect(game.score()).toBe(20);
+  });
+
+  it("should roll a spare", () => {
+    game = rollASpare(game);
+    game = game.roll(3);
+    game = rollMany(game, 17, 0);
+    expect(game.score()).toBe(16);
+  });
+
+  it("should roll a strike", () => {
+    game = game.roll(10).roll(3).roll(3);
+    game = rollMany(game, 17, 0);
+    expect(game.score()).toBe(22);
+  });
+
+  it("should do a perfect score", () => {
+    game = rollMany(game, 12, 10);
+    expect(game.score()).toBe(300);
+  });
 });
+
+function rollASpare(game: Game) {
+  return game.roll(5).roll(5);
+}
+
+function rollMany(game: Game, rolls: number, pins: number) {
+  for (let i = 0; i < rolls; i++) {
+    game = game.roll(pins);
+  }
+  return game;
+}
--- inclass/src/3_TDD/functional/bowlingGame.ts
+++ inclass/src/3_TDD/functional/bowlingGame.ts
@@ -1,22 +1,68 @@
 /* Remember that all your functions have to respect the functional programming rules:
- *  - 🔥 Pure functions and side effects 
+ *  - 🔥 Pure functions and side effects
  *  - 🔥 First class functions & higher order functions
  *  - 🔥 Immutability ⚠️
- *  - 🔥 Composition 
+ *  - 🔥 Composition
  *
- * See more info here: 
+ * See more info here:
  * https://www.learningjournal.guru/courses/scala/scala-programming-foundation/elements-of-functional-programming-1/
- * 
+ *
  * I'm putting a warning in Immutability to remember you that every time you try
  * to change the state var... just be carefull...⚠️
  */
 
-type State = { /* put params if needed */ }
-const INITIAL_STATE: State = {}
+type State = {
+  pins: number[];
+};
+const INITIAL_STATE: State = { pins: [] };
 
-export const createBowlingGame = (state: State = INITIAL_STATE) => Object.freeze({
-  //myFunction: () => callInnerModuleScopeFunction()
-  //myOhterFunction: (...) => myOtherFunction(...)
-})
+export const createBowlingGame = (state: State = INITIAL_STATE) =>
+  Object.freeze({
+    roll: (pins: number) => roll(state, pins),
+    score: () => score(state),
+  });
 
-//const myOtherFunction = (...) => {...}
+const roll = (state: State, pins: number) => {
+  return createBowlingGame({ pins: [...state.pins, pins] });
+};
+
+const score = ({ pins }: State) => {
+  let score = 0;
+  let firstTry = 0;
+  const FRAMES = 10;
+  for (let frame = 0; frame < FRAMES; frame++) {
+    if (isStrike(pins, firstTry)) {
+      score += scoreForStrike(pins, firstTry);
+      firstTry += 1;
+    } else if (isSpare(pins, firstTry)) {
+      score += scoreForSpare(pins, firstTry);
+      firstTry += 2;
+    } else {
+      score += scoreForFrame(pins, firstTry);
+      firstTry += 2;
+    }
+  }
+  return score;
+};
+
+export type Game = ReturnType<typeof createBowlingGame>;
+
+function isStrike(pins: number[], firstTry: number) {
+  return pins[firstTry] === 10;
+}
+
+function scoreForStrike(pins: number[], firstTry: number) {
+  return 10 + pins[firstTry + 1] + pins[firstTry + 2];
+}
+
+function scoreForFrame(pins: number[], firstTry: number) {
+  return pins[firstTry] + pins[firstTry + 1];
+}
+
+function scoreForSpare(pins: number[], firstTry: number) {
+  return pins[firstTry] + pins[firstTry + 1] + pins[firstTry + 2];
+}
+
+function isSpare(pins: number[], firstTry: number) {
+  return pins[firstTry] + pins[firstTry + 1] === 10;
+}
